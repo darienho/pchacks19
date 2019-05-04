16536a6d1cea05c5e58ba7e65c6a35ea951a9ab8
mazarie4 <joshua.mazariegosbobadilla@mail.utoronto.ca>
2019-05-04 17:19:59 -0400

    \aaaaaaaaaaaaa

--- node.js
+++ node.js
@@ -2,11 +2,41 @@
 // hosting on 8088
 
 var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use('/',express.static('static_files')); // this directory has files to be returned
+app.use(express.json());
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+var Storage = multer.diskStorage({ destination: function(req, file, callback) { callback(null, "./Images"); }, filename: function(req, file, callback) { callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname); } });
+var upload = multer({storage: Storage}).array("imgUploader", 3);
 
 // ********************************************
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/api/Upload", bodyParser.json({limit: "50mb"}), function(req, res) {
+	console.log(req);
+	upload(req, res, function(err) {
+		if (err) {
+			console.log(err);
+			return res.end("Something went wrong!");
+		}
+		console.log("i did it");
+		return res.end("File uploaded sucessfully!.");
+	});
+});
 
 // remove ingrediant
 app.put('/api/remove', function (req, res) {
@@ -19,8 +49,9 @@ app.put('/api/remove', function (req, res) {
 // remove ingrediant
 app.put('/api/add', function (req, res) {
 	var result = {};
-  	res.status();
+  	res.status(404);
 	res.json(result);
+	console.log(req.body.name);
 });
 
 app.listen(8088, function () {
